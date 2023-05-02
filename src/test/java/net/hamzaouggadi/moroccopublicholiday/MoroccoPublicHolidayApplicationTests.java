dec6a56d2f18dc186f5aa03e4ad410c18d0cb182
Hamza Ouggadi <45943796+HamzaOuggadi@users.noreply.github.com>
2023-05-02 12:23:43 +0100

    Initial commit

---
+++ src/test/java/net/hamzaouggadi/moroccopublicholiday/MoroccoPublicHolidayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hamzaouggadi.moroccopublicholiday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoroccoPublicHolidayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
